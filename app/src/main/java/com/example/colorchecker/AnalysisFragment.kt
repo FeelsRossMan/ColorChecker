5994bc586cebd552b28cb9fca046f8ba9f8619e4
Ross <25727163+FeelsRossMan@users.noreply.github.com>
2022-05-16 03:00:39 -0400

    Setup viewBinding on AnalysisFragment.kt

--- app/src/main/java/com/example/colorchecker/AnalysisFragment.kt
+++ app/src/main/java/com/example/colorchecker/AnalysisFragment.kt
@@ -1,5 +1,10 @@
 package com.example.colorchecker
 
-class AnalysisFragment {
+import androidx.fragment.app.Fragment
+import com.example.colorchecker.databinding.AnalysisFragmentBinding
+
+class AnalysisFragment: Fragment() {
+
+    private var _binding: AnalysisFragmentBinding? = null
 
 }
