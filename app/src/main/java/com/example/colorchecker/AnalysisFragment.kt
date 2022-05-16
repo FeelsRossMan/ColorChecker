4f1cbea582e83109fb3fcaa4b1e5abc0ec47a6c5
Ross <25727163+FeelsRossMan@users.noreply.github.com>
2022-05-16 03:06:27 -0400

    Setup empty onCreate, onViewCreated functions and added the onCreateView to setup viewBinding and the onDestroyView function to reset binding to null in the AnalysisFragment.kt file

--- app/src/main/java/com/example/colorchecker/AnalysisFragment.kt
+++ app/src/main/java/com/example/colorchecker/AnalysisFragment.kt
@@ -1,5 +1,9 @@
 package com.example.colorchecker
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.colorchecker.databinding.AnalysisFragmentBinding
 
@@ -7,4 +11,28 @@ class AnalysisFragment: Fragment() {
 
     private var _binding: AnalysisFragmentBinding? = null
 
+    private val binding get() = _binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = AnalysisFragmentBinding.inflate(inflater, container, false)
+        val view = binding?.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
