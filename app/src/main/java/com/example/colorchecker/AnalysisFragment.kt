11066721ebd735c5920794f82cd9e575e95b464c
Ross <25727163+FeelsRossMan@users.noreply.github.com>
2022-05-19 19:53:47 -0400

    Setup camera function and created empty viewmodel class

--- app/src/main/java/com/example/colorchecker/AnalysisFragment.kt
+++ app/src/main/java/com/example/colorchecker/AnalysisFragment.kt
@@ -1,20 +1,34 @@
 package com.example.colorchecker
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.colorchecker.databinding.AnalysisFragmentBinding
 
 class AnalysisFragment: Fragment() {
 
+    private lateinit var imageResultLauncher: ActivityResultLauncher<Intent>
     private var _binding: AnalysisFragmentBinding? = null
+    private lateinit var photoIV: ImageView
 
     private val binding get() = _binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        imageResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                handleCameraImage(result.data)
+            }
+        }
     }
 
     override fun onCreateView(
@@ -23,6 +37,11 @@ class AnalysisFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = AnalysisFragmentBinding.inflate(inflater, container, false)
+        _binding!!.openCameraFAB.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            imageResultLauncher.launch(cameraIntent)
+        }
+        photoIV = _binding!!.photoIV
         val view = binding?.root
         return view
     }
@@ -35,4 +54,11 @@ class AnalysisFragment: Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun handleCameraImage(intent: Intent?) {
+        val bitmap = intent?.extras?.get("data") as Bitmap
+        photoIV.setImageBitmap(bitmap)
+
+    }
+
 }
