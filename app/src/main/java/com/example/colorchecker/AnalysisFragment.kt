10c408729307136ce0fd6ba25dd44f164f3745af
Ross <25727163+FeelsRossMan@users.noreply.github.com>
2022-05-29 22:16:35 -0400

    Finally fixed the rotation issue using exif data on the photo.

--- app/src/main/java/com/example/colorchecker/AnalysisFragment.kt
+++ app/src/main/java/com/example/colorchecker/AnalysisFragment.kt
@@ -5,18 +5,22 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.media.ExifInterface
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -24,10 +28,12 @@ import com.example.colorchecker.databinding.AnalysisFragmentBinding
 import com.example.colorchecker.model.PhotoViewModel
 import java.io.File
 
+
 private const val FILE_NAME = "photo.jpg"
 private lateinit var photoFile: File
 
-class AnalysisFragment: Fragment() {
+
+class AnalysisFragment : Fragment() {
 
     private val model: PhotoViewModel by activityViewModels()
 
@@ -38,11 +44,12 @@ class AnalysisFragment: Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        imageResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                handleCameraImage()
+        imageResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    handleCameraImage(photoFile)
+                }
             }
-        }
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -55,11 +62,15 @@ class AnalysisFragment: Fragment() {
         _binding!!.openCameraFAB.setOnClickListener {
             photoFile = getPhotoFile()
             val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            val fileProvider = FileProvider.getUriForFile(this.requireContext(), "com.example.colorchecker.fileprovider", photoFile)
+            val fileProvider = FileProvider.getUriForFile(
+                this.requireContext(),
+                "com.example.colorchecker.fileprovider",
+                photoFile
+            )
             cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
             imageResultLauncher.launch(cameraIntent)
         }
-        _binding!!.photoIV.setOnTouchListener { _, motionEvent->  onTouchPhotoIV(motionEvent = motionEvent)}
+        _binding!!.photoIV.setOnTouchListener { _, motionEvent -> onTouchPhotoIV(motionEvent = motionEvent) }
         return binding?.root
     }
 
@@ -80,32 +91,75 @@ class AnalysisFragment: Fragment() {
         _binding = null
     }
 
-    private fun handleCameraImage() {
-        val bitmap = BitmapFactory.decodeFile(photoFile.absolutePath)
-        model.updateImageBitmap(bitmap, requireContext().display!!.rotation)
-        hideColorPopup()
+    private fun handleCameraImage(photoPath: File) {
+        val exifInterface = ExifInterface(photoPath.absolutePath)
+        val bitmap = BitmapFactory.decodeFile(photoPath.absolutePath)
+        val imageView = _binding?.photoIV
+        imageView?.let {
+            val x = imageView.measuredWidth
+            val y = imageView.measuredHeight
+            Log.d(LOG_TAG, "New bitmap with values width: $x, height: $y")
+            model.newImageBitmap(
+                bitmap,
+                x,
+                y,
+                exifInterface.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL
+                )
+            )
+        }
 
+        hideColorPopup()
     }
 
-    private fun onTouchPhotoIV(motionEvent: MotionEvent) : Boolean {
+    private fun onTouchPhotoIV(motionEvent: MotionEvent): Boolean {
         showColorPopup(motionEvent.x, motionEvent.y)
         return false
     }
 
     private fun showColorPopup(x: Float, y: Float) {
 
-        val layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-        layoutParams.setMargins(x.toInt(),y.toInt(),0,0)
-        val colorPopupView = _binding?.colorPopupRL
-        colorPopupView?.findViewById<TextView>(R.id.color_popup_ID)?.text = model.getColorStringOnClick(x,y)
-        model.getColorCodeOnClick(x,y)?.let {
-            colorPopupView?.findViewById<ImageView>(R.id.color_popup_IV)?.setBackgroundColor(it)
+        if (model.isNull()) {
+            Log.d(LOG_TAG, "showColorPopup called with null bitmap")
+            return
         }
+
+        val layoutParams = RelativeLayout.LayoutParams(
+            RelativeLayout.LayoutParams.WRAP_CONTENT,
+            RelativeLayout.LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.setMargins(x.toInt(), y.toInt(), 0, 0)
+        val colorPopupView = _binding?.colorPopupLL
+        colorPopupView?.findViewById<TextView>(R.id.color_popup_ID)?.text =
+            model.getColorStringOnClick(x, y)
+        model.getColorCodeOnClick(x, y).let {
+            colorPopupView?.findViewById<LinearLayout>(R.id.color_popup_LL)?.setBackgroundColor(it)
+            Log.d(LOG_TAG, "$it")
+            if (it < -10000000) {
+                colorPopupView?.findViewById<TextView>(R.id.color_popup_ID)?.setTextColor(ContextCompat.getColor(
+                    this.requireContext(), R.color.white))
+            } else {
+                colorPopupView?.findViewById<TextView>(R.id.color_popup_ID)?.setTextColor(ContextCompat.getColor(
+                    this.requireContext(), R.color.black))
+            }
+        }
+
         colorPopupView?.layoutParams = layoutParams
         colorPopupView?.visibility = View.VISIBLE
     }
 
     private fun hideColorPopup() {
-        _binding?.colorPopupRL?.visibility = View.INVISIBLE
+        _binding?.colorPopupLL?.visibility = View.INVISIBLE
+    }
+
+    private fun checkPopupLocationForWidth(x: Float,y: Float) : Boolean {
+
+
+        return true
+    }
+
+    companion object {
+        private const val LOG_TAG = "AnalysisFragment"
     }
 }
--- app/src/main/java/com/example/colorchecker/model/PhotoViewModel.kt
+++ app/src/main/java/com/example/colorchecker/model/PhotoViewModel.kt
@@ -2,43 +2,71 @@ package com.example.colorchecker.model
 
 import android.graphics.Bitmap
 import android.graphics.Matrix
+import android.media.ExifInterface
 import android.util.Log
-import android.view.Surface
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+
 
 class PhotoViewModel : ViewModel() {
     private var _imageBitmap = MutableLiveData<Bitmap>()
     val imageBitmap: LiveData<Bitmap>
         get() = _imageBitmap
 
-    fun updateImageBitmap(bitmap: Bitmap, rotation: Int) {
-        when (rotation) {
-            Surface.ROTATION_0 -> _imageBitmap.value = bitmap.rotate(90F)
-            Surface.ROTATION_180 -> _imageBitmap.value = bitmap.rotate(-90F)
+
+    fun newImageBitmap(newBitmap: Bitmap, ivWidth: Int, ivHeight: Int, rotate: Int = ExifInterface.ORIENTATION_NORMAL) {
+//        val bitmap = newBitmap.cropBitmapToSize(ivWidth, ivHeight)
+        var bitmap = newBitmap
+        Log.d(LOG_TAG, "$rotate")
+        when (rotate) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> bitmap = bitmap.rotate(90F)!!
+            ExifInterface.ORIENTATION_ROTATE_180 -> bitmap = bitmap.rotate(180F)!!
+            ExifInterface.ORIENTATION_ROTATE_270 -> bitmap = bitmap.rotate(270F)!!
             else -> _imageBitmap.value = bitmap
-
         }
-
+        _imageBitmap.value = Bitmap.createScaledBitmap(bitmap, ivWidth, ivHeight, true)
+        Log.d(LOG_TAG, "Created bitmap dimensions: width: ${bitmap.width} height: ${bitmap.height}")
     }
 
-    fun getColorStringOnClick(x: Float, y: Float) : String? {
-        val color = _imageBitmap.value?.getPixel(x.toInt(), y.toInt())
+//    fun updateImageBitmap(bitmap: Bitmap) {
+//        _imageBitmap.value = bitmap
+//    }
 
-        Log.d("PhotoViewModel", "Location: ($x, $y), Color: ($color)")
-        return color?.let { colorIntToString(it) }
+    fun getColorStringOnClick(x: Float, y: Float): String {
+        val color = getColorCodeOnClick(x, y)
+        Log.d(LOG_TAG, "Location: ($x, $y), Color: ($color)")
+
+        return colorIntToString(color)
     }
 
     private fun colorIntToString(colorInt: Int): String {
         return String.format("#%06X", 0xFFFFFF and colorInt)
     }
 
-    fun getColorCodeOnClick(x: Float, y: Float): Int? {
-        return _imageBitmap.value?.getPixel(x.toInt(), y.toInt())
+    fun getColorCodeOnClick(x: Float, y: Float): Int {
+        var returnColor = 0
+        try {
+            returnColor = _imageBitmap.value?.getPixel(x.toInt(), y.toInt())!!
+        } catch (err: IllegalArgumentException) {
+            Log.e(LOG_TAG, err.toString())
+        }
+        return returnColor
     }
+
     private fun Bitmap.rotate(degrees: Float): Bitmap? {
-        val matrix = Matrix().apply { postRotate(degrees)}
+        val matrix = Matrix().apply { postRotate(degrees) }
         return Bitmap.createBitmap(this, 0, 0, width, height, matrix, true)
+    }
+
+    fun isNull(): Boolean {
+        return (_imageBitmap.value == null)
+    }
+
+    //    private fun Bitmap.cropBitmapToSize(width: Int, height: Int) : Bitmap{
+//        return Bitmap.createBitmap(this, 0, 0, width, height)
+//    }
+    companion object {
+        private const val LOG_TAG = "PhotoViewModel"
     }
 }
